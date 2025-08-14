85e023973cf6c8741e305dac5501ff7e29958b02
lilong <18577171104@163.com>
2025-08-14 15:27:38 +0800

    修正提示词

--- tradingagents/agents/analysts/fundamentals_analyst.py
+++ tradingagents/agents/analysts/fundamentals_analyst.py
@@ -36,11 +36,11 @@ You are a Fundamentals Analyst. Your objective is to analyze fundamental informa
             [
                 (
                     "system",
-                    "You are a helpful AI assistant, collaborating with other assistants."
+                    "You are a helpful AI assistant"
                     " Use the provided tools to progress towards answering the question."
-                    " If you are unable to fully answer, that's OK; another assistant with different tools"
+                    " If you are unable to fully answer, that's OK; we have different tools"
                     " will help where you left off. Execute what you can to make progress."
-                    " If you or any other assistant has the FINAL TRANSACTION PROPOSAL: **BUY/HOLD/SELL** or deliverable,"
+                    " If you or any others has the FINAL TRANSACTION PROPOSAL: **BUY/HOLD/SELL** or deliverable,"
                     " prefix your response with FINAL TRANSACTION PROPOSAL: **BUY/HOLD/SELL** so the team knows to stop."
                     " You have access to the following tools: {tool_names}.\n{system_message}"
                     "For your reference, the current date is {current_date}. The company we want to look at is {ticker}",
